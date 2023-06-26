30997715f842d23735e0f83ce7e4f5c218aab919
buraktr03 <turanburak@gmail.com>
2023-06-26 23:04:46 +0300

    first reacaat project

---
+++ src/components/LessonCard/LessonCard.jsx
@@ -0,0 +1,14 @@
+
+const LessonCard = () => {
+  return (
+    <>
+    <img src="" alt="" />
+    <h3>Lesson Name:</h3>
+    <p></p>
+    <h3>Lesson Hour:</h3>
+    <p></p>
+    </>
+  )
+}
+
+export default LessonCard
---
+++ src/pages/Lesson.jsx
@@ -0,0 +1,12 @@
+import LessonCard from "../components/LessonCard/LessonCard"
+
+const Lesson = () => {
+  return (
+    <>
+    <h2>Lesson Reminder</h2>
+    <LessonCard/>
+    </>
+  )
+}
+
+export default Lesson
